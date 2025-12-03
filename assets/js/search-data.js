121380ace382b7918ebe95279b5b157d351a8df7
rizanB <48565764+rizanB@users.noreply.github.com>
2025-12-03 08:09:11 +0000

    Deploying to gh-pages from @ rizanB/rizanb.github.io@731e2ea6c9fce64908c952395a1d0d55aa952458 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/books/";
               },
+            },{id: "dropdown-bioinformatics",
+              title: "bioinformatics",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "https://bioinfo.acchyut.com.np";
+              },
             },{id: "post-a-gentle-introduction-to-feature-scaling",
         
           title: 'A Gentle Introduction to Feature Scaling <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
